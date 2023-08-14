c1636186a6fdfe339f5b36c1e4eeebe6e7243975
artem <Artem Antonov>
2023-08-14 14:20:12 +0300

    0.0.5

--- src/App.vue
+++ src/App.vue
@@ -15,69 +15,42 @@ export default {
   mounted() {
     const scene = new THREE.Scene();
     const aspect = window.innerWidth / window.innerHeight;
-    const frustumSize = 20;
-    const camera = new THREE.OrthographicCamera(frustumSize * aspect / -2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / -2, 0.1, 1000);
+    const frustumSize = 50;
+
+    const camera = new THREE.OrthographicCamera(- 3*aspect,  3*aspect, 3, -3, 0.1, 1000);
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor(0x000000);
+
     document.getElementById('cubeContainer').appendChild(renderer.domElement);
 
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshBasicMaterial({ color: 0xf056b1, transparent: true, opacity: 0.5 });
 
     const cube = new THREE.Mesh(geometry, material);
-
     scene.add(cube);
 
-    camera.position.z = 7;
+    camera.position.z = 1;
 
+    /* REMARK: Вот так можно привязать сетку к камере */
     const gridGroup = new GridGroup(config);
-    scene.add(gridGroup);
+    camera.add(gridGroup);
 
-    gridGroup.scale(frustumSize)
+    scene.add(camera);
 
-    const controls = new OrbitControls(camera, renderer.domElement);
+    gridGroup.rescale(frustumSize)
 
-    let isDragging = false;
-    let previousMousePosition = { x: 0, y: 0 };
-    
-    document.addEventListener('mousedown', function (e) {
-      isDragging = true;
+    const controls = new OrbitControls( camera, renderer.domElement );
+
+    controls.update();
+
+    document.addEventListener('wheel', function (e) {
+        // TODO: здесь прописать условия рескейлинга сетки
     });
-    
-    document.addEventListener('mousemove', function (e) {
-      const deltaMove = {
-        x: e.offsetX - previousMousePosition.x,
-        y: e.offsetY - previousMousePosition.y
-      };
-
-      if (isDragging) {
-        const deltaRotationQuaternion = new THREE.Quaternion()
-          .setFromEuler(new THREE.Euler(
-            toRadians(deltaMove.y * 1),
-            toRadians(deltaMove.x * 1),
-            0,
-            'XYZ'
-          ));
-        cubeGroup.quaternion.multiplyQuaternions(deltaRotationQuaternion, cubeGroup.quaternion);
-      }
-
-      previousMousePosition = { x: e.offsetX, y: e.offsetY };
-    });
-    
-    document.addEventListener('mouseup', function (e) {
-      isDragging = false;
-    });
-
-    function toRadians(angle) {
-      return angle * (Math.PI / 180);
-    }
 
     const animate = function () {
       requestAnimationFrame(animate);
-
-      gridGroup.lookAt(camera.position);
 
       renderer.render(scene, camera);
     };
@@ -86,4 +59,4 @@ export default {
 
 }, }; </script>
 
-<style> #cubeContainer { width: 100%; height: 100%; } </style>
+// <style> #cubeContainer { width: 100%; height: 100%; } </style>
--- src/GridGroup.js
+++ src/GridGroup.js
@@ -1,6 +1,8 @@
 import {Group} from "three";
 import * as THREE from 'three';
 
+const DEPTH = -5
+
 export default class GridGroup extends Group {
 
     constructor(config) {
@@ -11,44 +13,44 @@ export default class GridGroup extends Group {
 
     }
 
-
-    scale(frustumSize){
+    
+    rescale(frustumSize){
         /** Поведение при ремасштабировании */
 
         for (const name in this.config.grid) {
             const lineConfig = this.config.grid[name]
             const {divisionValue, size, color} = lineConfig;
 
-
-            // const lineSegment = new THREE.LineSegments()
+            /* TODO: Здесь нужно использовать на каждый тип линий не множество THREE.Line а ровно один  THREE.LineSegments */
+            /* REMARK: Я убрал if divisionValue === 0 за ненадобностью и упростил код цикла */
 
             const lineMaterial = new THREE.LineBasicMaterial({ color: parseInt(color), linewidth: size });
 
-            if (divisionValue === 0) {
-                const verticalLinePoints = [new THREE.Vector3(0, -frustumSize / 2, -1), new THREE.Vector3(0, frustumSize / 2, -1)];
+            for (let position = 0; position <= frustumSize; position += divisionValue){
+                const verticalLinePoints = [new THREE.Vector3(position, -frustumSize, DEPTH), new THREE.Vector3(position, frustumSize, DEPTH)];
                 const verticalLineGeometry = new THREE.BufferGeometry().setFromPoints(verticalLinePoints);
                 const verticalLine = new THREE.Line(verticalLineGeometry, lineMaterial);
                 this.add(verticalLine);
-            
-                const horizontalLinePoints = [new THREE.Vector3(-frustumSize / 2, 0, -1), new THREE.Vector3(frustumSize / 2, 0, -1)];
+
+                const horizontalLinePoints = [new THREE.Vector3(-frustumSize, position, DEPTH), new THREE.Vector3(frustumSize, position, DEPTH)];
                 const horizontalLineGeometry = new THREE.BufferGeometry().setFromPoints(horizontalLinePoints);
                 const horizontalLine = new THREE.Line(horizontalLineGeometry, lineMaterial);
                 this.add(horizontalLine);
-            } else {
-                for (let position = -frustumSize * 2 / 2; position <= frustumSize*2  / 2; position += divisionValue) {
-                    const verticalLinePoints = [new THREE.Vector3(position, -frustumSize / 2, -1), new THREE.Vector3(position, frustumSize / 2, -1)];
-                    const verticalLineGeometry = new THREE.BufferGeometry().setFromPoints(verticalLinePoints);
-                    const verticalLine = new THREE.Line(verticalLineGeometry, lineMaterial);
-                    this.add(verticalLine);
-                
-                    if (position >= -frustumSize / 2 && position <= frustumSize / 2) {
-                        const horizontalLinePoints = [new THREE.Vector3(-frustumSize / 2, position, -1), new THREE.Vector3(frustumSize / 2, position, -1)];
-                        const horizontalLineGeometry = new THREE.BufferGeometry().setFromPoints(horizontalLinePoints);
-                        const horizontalLine = new THREE.Line(horizontalLineGeometry, lineMaterial);
-                        this.add(horizontalLine);
-                    }
-                }
             }
+
+            for (let position = 0; position >= -frustumSize; position -= divisionValue){
+                const verticalLinePoints = [new THREE.Vector3(position, -frustumSize, DEPTH), new THREE.Vector3(position, frustumSize, DEPTH)];
+                const verticalLineGeometry = new THREE.BufferGeometry().setFromPoints(verticalLinePoints);
+                const verticalLine = new THREE.Line(verticalLineGeometry, lineMaterial);
+                this.add(verticalLine);
+
+                const horizontalLinePoints = [new THREE.Vector3(-frustumSize, position, DEPTH), new THREE.Vector3(frustumSize, position, DEPTH)];
+                const horizontalLineGeometry = new THREE.BufferGeometry().setFromPoints(horizontalLinePoints);
+                const horizontalLine = new THREE.Line(horizontalLineGeometry, lineMaterial);
+                this.add(horizontalLine);
+            }
+
+
         }
 
     }
